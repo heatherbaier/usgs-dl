4c7cae77b0226cd1138fb700e8fc62839c7edbee
heatherbaier <zhonghui.lv@gmail.com>
2023-07-13 18:27:52 +0000

    initial commit

---
+++ dl_temp.py
@@ -0,0 +1,105 @@
+from landsatxplore.earthexplorer import EarthExplorer
+from shapely.geometry import Polygon
+from landsatxplore.api import API
+from rasterio.merge import merge
+from rasterio.plot import show
+import rioxarray as riox
+from pathlib import Path
+import geopandas as gpd
+import rasterio as rio
+import pandas as pd
+import argparse
+import tarfile
+import shapely
+# import pygee
+import os
+
+from utils import *
+
+
+
+# import geopandas as gpd
+import calendar
+# import pygee
+import os
+
+# from utils import *
+
+
+def chunker_list(seq, size):
+    return (seq[i::size] for i in range(size))
+
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('ISO', type = str)
+    parser.add_argument('year', type = int)
+    args = parser.parse_args()
+    
+#     for year in range(args.year - 5, args.year):
+        
+    for month in range(4, 13):
+
+        all_dates = [str(1996), str(month)]
+
+        print(all_dates)
+
+        base_dir = f"/rapids/notebooks/sciclone/geograd/Heather/clean_dl/imagery/{args.ISO}"
+
+        dates = GetDays(all_dates[0], all_dates[1])
+
+        download_dir = f"{base_dir}/{all_dates[0]}/{all_dates[1]}/zips"
+        unzip_dir = f"{base_dir}/{all_dates[0]}/{all_dates[1]}/unzips"
+        mosaic_dir = f"{base_dir}/{all_dates[0]}/{all_dates[1]}/mosaics"    
+        year_dir = f"{base_dir}/{all_dates[0]}"  
+        month_dir = f"{base_dir}/{all_dates[0]}/{all_dates[1]}"    
+
+        print(all_dates, download_dir, unzip_dir, mosaic_dir, "\n")
+
+        if not os.path.exists(year_dir):
+            os.mkdir(year_dir)
+
+        if not os.path.exists(month_dir):
+            os.mkdir(month_dir)
+
+        if not os.path.exists(download_dir):
+            os.mkdir(download_dir)
+
+        if not os.path.exists(unzip_dir):
+            os.mkdir(unzip_dir)
+
+        if not os.path.exists(mosaic_dir):
+            os.mkdir(mosaic_dir)
+
+        shp = gpd.read_file("/rapids/notebooks/sciclone/geograd/Heather/clean_dl/shps/NPL/geo2_np2001_2011.shp")
+        shp["centroid"] = shp.geometry.centroid
+        shp["bbox"] = shp["geometry"].apply(lambda x: calc_bbox(x))
+        shp.head()
+
+        # Your USGS  credentials
+        username = "hmbaier"
+        password = "930941741Hb."
+
+        ee = EarthExplorer(username, password)
+
+        # Initialize a new API instance
+        api = API(username, password)
+
+        # Perform a request
+        response = api.request(endpoint="dataset-catalogs")
+        print(response)
+
+        scenes = search_for_imagery(api, shp, ["landsat_tm_c2_l1"], dates[0], dates[1])
+        print(scenes.head())
+
+        scenes.to_csv(f"{args.ISO}_{all_dates[0]}_{all_dates[1]}_scenes.csv", index = False)
+
+        download_imagery(ee, scenes, download_dir)
+
+        untar_imagery(download_dir, unzip_dir)
+
+        mosaic_imagery(scenes, mosaic_dir, unzip_dir, shp, all_dates)         
+        
+        
---
+++ utils.py
@@ -0,0 +1,188 @@
+from landsatxplore.earthexplorer import EarthExplorer
+from shapely.geometry import Polygon
+from landsatxplore.api import API
+from rasterio.merge import merge
+from rasterio.plot import show
+import rioxarray as riox
+from pathlib import Path
+import geopandas as gpd
+import rasterio as rio
+import pandas as pd
+import calendar    
+import tarfile
+import shapely
+import os
+
+
+def calc_bbox(x):
+    return shapely.geometry.box(*x.bounds, ccw=True)
+
+
+def search_for_imagery(api, shp, ics, date1, date2):
+    
+    all_scenes = []
+
+    for col, row in shp.iterrows():
+
+        df_scenes = None
+
+        try:
+
+            for ic in ics:#, "landsat_tm_c2_l2"]:
+
+                lng = row.centroid.x
+                lat = row.centroid.y
+                
+#                 lng = row.geometry.centroid.x
+#                 lat = row.geometry.centroid.y                
+
+                scenes = api.search(
+                    dataset = ic,
+                    latitude = lat,
+                    longitude = lng,
+                    start_date = date1,
+                    end_date = date2,
+                    max_cloud_cover = 50
+                )
+
+                # Create a DataFrame from the scenes
+                df_scenes = pd.DataFrame(scenes)
+                df_scenes = df_scenes[['display_id','wrs_path', 'wrs_row','satellite','cloud_cover','acquisition_date']]
+                df_scenes.sort_values('acquisition_date', ascending=False, inplace=True)
+                df_scenes["shapeID"] = row.GEOLEVEL2
+                df_scenes["ic"] = ic
+
+                all_scenes.append(df_scenes)
+
+        except:
+
+            print(row.GEOLEVEL2, " has no imagery.")
+
+    all_scenes = pd.concat(all_scenes)
+
+    return all_scenes
+
+
+# scenes = search_for_imagery(api, shp, ["landsat_tm_c2_l1"])
+
+
+def download_imagery(ee, all_scenes, output_dir):
+    
+    for display_id in all_scenes["display_id"].unique():
+
+        try:
+
+            ee.download(display_id, output_dir = output_dir)
+
+            print(display_id)
+
+        except:
+
+            pass
+    
+# download_imagery(scenes, "data2")
+
+
+def untar_imagery(data_dir, output_dir):
+    
+    for file in os.listdir(data_dir):
+
+        if ".ipynb" not in file:
+
+            print(file)
+            
+            try:
+
+                tar = tarfile.open('{}/{}'.format(data_dir, file))
+                tar.extractall('{}/{}'.format(output_dir, file.replace(".tar", "")))
+                tar.close()
+            
+            except:
+                
+                pass
+            
+            
+            
+def mosaic_imagery(all_scenes, output_dir, unzip_dir, shp, all_dates, bands = ["B1", "B2", "B3"]):
+    
+    for shapeID in all_scenes["shapeID"].unique():
+        
+        try:
+
+            cur = all_scenes[all_scenes["shapeID"] == shapeID]["display_id"].unique()
+    #         print(cur)
+
+            shapePath = os.path.join(output_dir, shapeID)
+
+            raster_files = []
+
+            if not os.path.exists(shapePath):
+                os.mkdir(shapePath)
+
+            for band in bands:
+
+                raster_files = []
+
+                for file in cur:
+
+                    raster_files.append(f"{unzip_dir}/{file}/{file}_{band}.TIF")
+
+    #             print(raster_files, "\n")
+
+                raster_to_mosiac = []
+
+                for p in raster_files:
+                    raster = rio.open(p)
+                    raster_to_mosiac.append(raster)    
+
+                mosaic, output = merge(raster_to_mosiac)
+
+                output_path = f"{shapePath}/{shapeID}_{band}.tiff"
+
+                output_meta = raster.meta.copy()
+                output_meta.update(
+                    {"driver": "GTiff",
+                        "height": mosaic.shape[1],
+                        "width": mosaic.shape[2],
+                        "transform": output,
+                    }
+                )
+
+                with rio.open(output_path, "w", **output_meta) as m:
+                    m.write(mosaic)
+
+                # Read raster using rioxarray
+                raster = riox.open_rasterio(output_path)
+                raster = raster.rio.reproject(shp.crs)
+
+                # Shapely Polygon  to clip raster
+                geom = shp[shp["GEOLEVEL2"] == shapeID]["bbox"].squeeze()
+
+                # Use shapely polygon in clip method of rioxarray object to clip raster
+                clipped_raster = raster.rio.clip([geom])
+
+                # Save clipped raster
+                clipped_raster.rio.to_raster(f"{shapePath}/{shapeID}_{all_dates[0]}_{all_dates[1]}_{band}_clipped.tiff")   
+
+                os.remove(output_path)
+                
+        except:
+            
+            pass
+            
+
+            
+# mosaic_imagery(scenes, "saved2", "unzipped")         
+
+
+def GetDays(year, month):
+    
+    if month != 'all':
+        r = list(calendar.monthrange(int(year), int(month)))[1]
+        sdate = "-".join([str(year), str(month), str(1)])
+        edate = "-".join([str(year), str(month), str(r)])
+        return [sdate, edate]
+    else:
+        sdate = str(year) + "-" + str(1) + "-" + str(1)
+        edate = str(year) + "-" + str(12) + "-" + str(31)
+        return [sdate, edate]
